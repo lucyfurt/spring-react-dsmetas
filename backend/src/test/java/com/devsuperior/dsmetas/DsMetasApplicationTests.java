23d729b5d1df317eb6ca1c3ce63f5a8f56ccddca
lucyfurt <lucianafurtado10@gmail.com>
2022-07-15 21:42:12 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/dsmetas/DsMetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
